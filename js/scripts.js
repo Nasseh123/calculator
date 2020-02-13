a1a69c4a4b02c55f9f9295555b70a9f2df8ee8dc
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-02-13 11:54:00 +0300

    Adition of BMI CALCULATOR

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
-var add =function(number1,number2){
-    return number1 + number2;
+  //Addition Calculator
+  var add =function(number1,number2){
+        return number1 + number2;
+        };
+        var number1=parseInt(prompt("Enter NUmber:"));
+        var number2=parseInt(prompt("Enter another number"));
+        alert(add(number1,number2));
+
+//BMI Calculator
+var add =function(weight,height){
+    return weight / height;
     };
-    var number1=parseInt(prompt("Enter NUmber:"));
-    var number2=parseInt(prompt("Enter another number"));
-    alert(add(number1,number2));
+    var weight=parseInt(prompt("Enter Weight in kgs:"));
+    var height=parseFloat (prompt("Enter height in meters"));
+    alert(add(weight,height));
